17e72ac8d70fa40d74c6cf8c5394b250419b6030
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-09-26 16:38:23 +0200

    [feature]: WS-quete3-binding-amelioration affichage article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,35 @@
+
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Article {
+  title: string;
+  author: string;
+  content: string;
+  image: string;
+  isPublished: boolean;
+  comment: string;
+}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+
+  imports: [FormsModule],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+  styleUrl: './article.component.scss',
 })
 export class ArticleComponent {
+  article: Article= {
+    title: "Titre de l'article",
+    author: 'John Doe',
+    content: "Voici le contenu de l'article.",
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: '',
+  };
 
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
